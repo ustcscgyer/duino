4e58675342720708b752bb5a771efd6f082a74aa
Giovanni Ge <ustcscgyer@gmail.com>
2017-09-20 17:15:20 -0700

    Update clocktower.py
    The intra_time_diff algorithm is vectorized and not 10^6 times faster.

--- clocktower.py
+++ clocktower.py
@@ -1,7 +1,7 @@
 """
 Author: Giovanni Ge
 Created: 2017/03/30
-Last Modification: 2017/07/12
+Last Modification: 2017/09/12
 
 This module provides various of objects and methods related to time
 """
@@ -38,9 +38,10 @@ class USFinancialHolidayCalendar(AbstractHolidayCalendar):
     ]
 
 # US business day
-us_bd = CustomBusinessDay(calendar = USFederalHolidayCalendar())
+us_bd = CustomBusinessDay(calendar=USFederalHolidayCalendar())
 # US financial business day
-fi_bd = CustomBusinessDay(calendar = USFinancialHolidayCalendar())
+fi_bd = CustomBusinessDay(calendar=USFinancialHolidayCalendar())
+fi_holidays = USFinancialHolidayCalendar().holidays
 # US Exchange bussiness hour
 fi_bh = CustomBusinessHour(calendar=USFederalHolidayCalendar(), start='9:30', end='16:30')
 
@@ -63,7 +64,6 @@ def weekday_distance(t1, t2, convention='forward'):
 
     # Portions that is more than one week
     n0 = (t2.toordinal() - t1.toordinal()) // 7 * 5
-    print(n0)
     # Portions that is smaller than one week
     wd1, wd2 = t1.weekday(), t2.weekday()
     # Mathematical magic
@@ -80,7 +80,7 @@ def weekday_distance(t1, t2, convention='forward'):
 
     return n0 + n1
 
-def cbday_distance(t1, t2, holidays = None, convention = 'forward'):
+def cbday_distance(t1, t2, holidays=fi_holidays, convention='forward'):
     """ Number of custom business days between t1 and t2: t2 - t1
     Rolling forward convention: from 0 am+ to 0 am+
     example: Friday to Saturday is 1
@@ -96,51 +96,79 @@ def cbday_distance(t1, t2, holidays = None, convention = 'forward'):
     convention: str, 'forward' or 'backward'
     """
     # First calculate number of weekdays
-    wkd_dis = weekday_distance(t1,t2,convention)
+    if t1 > t2:
+        return -1 * cbday_distance(t2, t1, holidays, convention)
+
+    wkd_dis = weekday_distance(t1, t2, convention)
     if holidays is None: return wkd_dis
 
     # Calculate number of holidays that is not weekends
-    if convention == 'forward':
-        dates = holidays[(holidays >= t1) & (holidays < t2)]
-    elif convention == 'backward':
-        dates = holidays[(holidays > t1) & (holidays <= t2)]
-    else:
-        raise ValueError('wrong convetion')
+    dates = holidays(t1, t2)
+
+    # if convention == 'forward':
+    #     # dates = holidays[(holidays >= t1) & (holidays < t2)]
+    #     dates = holidays(t1,t2)
+    # elif convention == 'backward':
+    #     dates = holidays(t1,t2)
+    #     # dates = holidays[(holidays > t1) & (holidays <= t2)]
+    # else:
+    #     raise ValueError('wrong convetion')
 
     nh = len([d for d in dates if d.weekday() <= 4])
     
     return wkd_dis - nh
     
-def bizday_diff(t1,t2):
+def bizday_distance(t1, t2, offset=fi_bd):
     """ For given two datetime, this module calculates their difference in number of bussiness
     days.
-    i.g.: bizday_diff(dt.datetime(2017,1,5), dt.datetime(2017,1,4)) = 1
+    i.g.: bizday_diff(dt.datetime(2017,1,4), dt.datetime(2017,1,5)) = 1
     """
-    if t1>t2:
-        dday = max(len(pd.bdate_range(t2,t1,freq=fi_bd)) - 1, 0)
+    if t1 < t2:
+        dday = max(len(pd.bdate_range(t1, t2, freq=offset)) - 1, 0)
     else:
-        dday = min(1 - len(pd.bdate_range(t1,t2,freq=fi_bd)),0)
+        dday = min(1 - len(pd.bdate_range(t2, t1, freq=offset)),0)
     return dday
 
-def intra_time_diff(t1,t2, mrkt_open_time = dt.time(9,30), mrkt_close_time = dt.time(16,0)):
+def intra_time_diff(t1, t2, mrkt_open_time=dt.time(9,30), mrkt_close_time=dt.time(16,0), holidays=fi_holidays):
     """ For given two datetime, this module calculates their difference in minutes, 
     if two inputs are from different days, each day if treated as number of minutes between 
     mrkt_open_time and mrkt_close_time. 
     The time difference is adjusted for weekends and holidays
-    i.g.: intra_time_diff(dt.datetime(2017,1,5,10), dt.datetime(2017,1,4,9)) = 450
+    i.g.: intra_time_diff(dt.datetime(2017,1,4,9), dt.datetime(2017,1,5,10)) = 450
         (1day == 390 + 1 hour == 60 ==> 450)
+
+    This new imiplementation is 10^6 times faster than the previous one!!! 
     """
-    t1_time, t2_time = t1.time(), t2.time()
-    if t1_time < mrkt_open_time: t1_time = mrkt_open_time
-    if t1_time > mrkt_close_time: t1_time = mrkt_close_time
-    if t2_time < mrkt_open_time: t2_time = mrkt_open_time
-    if t2_time > mrkt_close_time: t2_time = mrkt_close_time
+    # Just for convenience
+    if not isinstance(t1, pd.Series):
+        t1 = pd.Series(t1)
+    if not isinstance(t2, pd.Series):
+        t2 = pd.Series(t2)
+
+    t1 = pd.concat([t1.rename('datetime'), t1.dt.time.rename('time'), t1.dt.date.rename('date'), 
+                t1.dt.hour.rename('hour'), t1.dt.minute.rename('minute')], axis=1)
+    t2 = pd.concat([t2.rename('datetime'), t2.dt.time.rename('time'), t2.dt.date.rename('date'), 
+                t2.dt.hour.rename('hour'), t2.dt.minute.rename('minute')], axis=1)
+
+    t1.loc[t1.time < mrkt_open_time, ['time', 'hour', 'minute']] = \
+        [mrkt_open_time, mrkt_open_time.hour, mrkt_open_time.minute]
+    t1.loc[t1.time > mrkt_close_time, ['time', 'hour', 'minute']] = \
+        [mrkt_close_time, mrkt_close_time.hour, mrkt_close_time.minute]
+    t2.loc[t2.time < mrkt_open_time, ['time', 'hour', 'minute']] = \
+        [mrkt_open_time, mrkt_open_time.hour, mrkt_open_time.minute]
+    t2.loc[t2.time > mrkt_close_time, ['time', 'hour', 'minute']] = \
+        [mrkt_close_time, mrkt_close_time.hour, mrkt_close_time.minute]
+
+    if len(t1) == 1: t1 = t1.iloc[0]
+    if len(t2) == 1: t2 = t2.iloc[0]
 
     minutes_of_day = 60*(mrkt_close_time.hour - mrkt_open_time.hour) + \
         (mrkt_close_time.minute - mrkt_open_time.minute)
-    if t1>t2:
-        dday = max(len(pd.bdate_range(t2,t1,freq=fi_bd)) - 1, 0)
+
+    if isinstance(t1, pd.Series) & isinstance(t2, pd.Series):
+        input_table = pd.DataFrame({'date1':t1.date, 'date2':t2.date}, index=[0])
     else:
-        dday = min(1 - len(pd.bdate_range(t1,t2,freq=fi_bd)),0)
-    return minutes_of_day*dday + 60*(t1_time.hour - t2_time.hour)\
-        +(t1_time.minute - t2_time.minute)
+        input_table = pd.DataFrame({'date1':t1.date, 'date2':t2.date})
+    dday = input_table.apply(lambda x: cbday_distance(x.date1, x.date2, holidays), axis=1)
+
+    return minutes_of_day*dday + 60*(t2.hour - t1.hour) + (t2.minute - t1.minute)
