9d5f5e2e58241adbc4f39eb5c70a76aaa714970a
Giovanni Ge <ustcscgyer@gmail.com>
2017-09-27 16:17:43 -0700

    Update pandas_ext.py
    extend pandas_ext.null_or_empty for Series

--- pandas_ext.py
+++ pandas_ext.py
@@ -5,4 +5,9 @@ This module extends some useful functionality to pandas
 import pandas as pd
 
 def null_or_empty(df):
-    return df.isnull() | df.applymap(lambda x: x=='')
+    if isinstance(df, pd.DataFrame):
+        return df.isnull() | df.applymap(lambda x: x=='')
+    elif isinstance(df, pd.Series):
+        return df.isnull() | (df=='')
+    else:
+        raise ValueError("No implemented!")
